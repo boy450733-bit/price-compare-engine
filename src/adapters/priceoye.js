4297f55a6e4531f2fa115f3c6e7fc3d233bd13e4
boy450733-bit <boy450733@gmail.com>
2026-07-19 01:32:04 +0500

    Delete src/adapters/priceoye.js

--- src/adapters/priceoye.js
+++
@@ -1,4 +0,0 @@
-//import { createAdapter } from "./createAdapter.js";
-//import { priceoyeConfig } from "./stores/priceoye.config.js";
-
-//export const priceoyeAdapter = createAdapter(priceoyeConfig);
