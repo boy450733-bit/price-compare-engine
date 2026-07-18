0124f611616a3617334966cc7430cd88625a31fe
boy450733-bit <boy450733@gmail.com>
2026-07-19 01:31:52 +0500

    Delete src/adapters/iShopping.js

--- src/adapters/iShopping.js
+++
@@ -1,4 +0,0 @@
-import { createAdapter } from "./createAdapter.js";
-import { iShoppingConfig } from "./stores/ishopping.config.js";
-
-export const iShoppingAdapter = createAdapter(iShoppingConfig);
