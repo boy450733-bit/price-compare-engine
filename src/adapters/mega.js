4f6850ad02fed6f50ed8f25d5f92be19dc7dcd1c
boy450733-bit <boy450733@gmail.com>
2026-07-18 23:56:41 +0500

    Update mega.js

--- src/adapters/mega.js
+++ src/adapters/mega.js
@@ -1,23 +1,4 @@
 import { createAdapter } from "./createAdapter.js";
-import { genericAdapter } from "./generic.js";
 import { megaConfig } from "./stores/mega.config.js";
-// import { darazConfig } from "./stores/daraz.config.js";
-// import { priceOyeConfig } from "./stores/priceoye.config.js";
 
-// Adding a new store = add its config import above, then one line below.
-// No other file needs to change.
-const configs = {
-  "Mega.pk": megaConfig,
-  // "Daraz": darazConfig,
-  // "PriceOye": priceOyeConfig,
-};
-
-const adapterCache = {};
-
-export function getAdapter(storeName) {
-  if (!configs[storeName]) return genericAdapter;
-  if (!adapterCache[storeName]) {
-    adapterCache[storeName] = createAdapter(configs[storeName]);
-  }
-  return adapterCache[storeName];
-}
+export const megaAdapter = createAdapter(megaConfig);
