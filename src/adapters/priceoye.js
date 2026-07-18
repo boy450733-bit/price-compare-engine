d11349db4f5b5ba547f57e771d4deaf56e80febb
boy450733-bit <boy450733@gmail.com>
2026-07-19 01:26:35 +0500

    Update priceoye.js

--- src/adapters/priceoye.js
+++ src/adapters/priceoye.js
@@ -1,4 +1,4 @@
-import { createAdapter } from "./createAdapter.js";
-import { priceoyeConfig } from "./stores/priceoye.config.js";
+//import { createAdapter } from "./createAdapter.js";
+//import { priceoyeConfig } from "./stores/priceoye.config.js";
 
-export const priceoyeAdapter = createAdapter(priceoyeConfig);
+//export const priceoyeAdapter = createAdapter(priceoyeConfig);
