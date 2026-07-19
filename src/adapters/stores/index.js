0112f96bad796ca0e6187a4fad0128d924df7c49
boy450733-bit <boy450733@gmail.com>
2026-07-19 09:44:18 +0500

    Update index.js

--- src/adapters/stores/index.js
+++ src/adapters/stores/index.js
@@ -1,9 +1,11 @@
 import { megaConfig } from "./mega.config.js";
 import { priceOyeConfig } from "./priceoye.config.js";
 import { iShoppingConfig } from "./ishopping.config.js";
+import { darazConfig } from "./daraz.config.js";
 
 export const allStoreConfigs = [
   megaConfig,
   priceOyeConfig,
   iShoppingConfig,
+  darazConfig,
 ];
