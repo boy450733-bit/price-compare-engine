bfd423c5768ff518fa99e4996fec1ecee1aea343
boy450733-bit <boy450733@gmail.com>
2026-07-19 10:50:53 +0500

    Update index.js

--- src/adapters/stores/index.js
+++ src/adapters/stores/index.js
@@ -2,10 +2,12 @@ import { megaConfig } from "./mega.config.js";
 import { priceOyeConfig } from "./priceoye.config.js";
 import { iShoppingConfig } from "./ishopping.config.js";
 import { darazConfig } from "./daraz.config.js";
+import { flashiConfig } from "./flashi.config.js";
 
 export const allStoreConfigs = [
   megaConfig,
   priceOyeConfig,
    // iShoppingConfig,  // disabled: Cloudflare managed challenge as of 2026-07-19
   darazConfig,
+  flashiConfig,
 ];
