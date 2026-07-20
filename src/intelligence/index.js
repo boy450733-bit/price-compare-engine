60775cc7515a3e371c12d42cb5bdae606cd53107
boy450733-bit <boy450733@gmail.com>
2026-07-20 23:11:29 +0500

    Create index.js

---
+++ src/intelligence/index.js
@@ -0,0 +1,9 @@
+// src/intelligence/index.js
+
+export * from "./brands.js";
+export * from "./category.js";
+export * from "./fingerprint.js";
+export * from "./normalizer.js";
+export * from "./pipeline.js";
+export * from "./relevance.js";
+export * from "./specExtractor.js";
