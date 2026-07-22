b6b8cf4e6974e07d59ec29cb3dda3155e023a6dd
boy450733-bit <boy450733@gmail.com>
2026-07-22 23:02:58 +0500

    Update index.js

--- src/intelligence/index.js
+++ src/intelligence/index.js
@@ -1,5 +1,5 @@
 // src/intelligence/index.js
-
+/*
 export * from "./brands.js";
 export * from "./category.js";
 export * from "./fingerprint.js";
@@ -7,3 +7,23 @@ export * from "./normalizer.js";
 export * from "./pipeline.js";
 export * from "./relevance.js";
 export * from "./specExtractor.js";
+*/
+export function processProduct(listing, searchQuery, storeName) {
+  return {
+    ...listing,
+
+    accept: true,
+
+    brand: "TEST_BRAND",
+    model: "TEST_MODEL",
+    category: "mobile",
+
+    specs: {
+      ram: "8GB",
+      storage: "256GB",
+      color: "Black"
+    },
+
+    fingerprint: "TEST_FINGERPRINT_123456"
+  };
+}
