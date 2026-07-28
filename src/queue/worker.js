26e3269806c58b8d39c8028d8e88b7585d7ce23d
boy450733-bit <boy450733@gmail.com>
2026-07-29 00:24:24 +0500

    worker.js updated

--- src/queue/worker.js
+++ src/queue/worker.js
@@ -1,6 +1,8 @@
 import { Worker } from "bullmq";
+import cron from "node-cron";
 import { connection } from "./queue.js";
 import { scrapeAllStoresForQuery } from "../scraper/scrape.js";
+import { checkAndSendPriceAlerts } from "../utils/notifier.js";
 
 // Run this as a separate process: `npm run worker`
 const worker = new Worker(
@@ -18,4 +20,11 @@ worker.on("failed", (job, err) => {
   console.error(`[worker] job ${job.id} failed:`, err.message);
 });
 
+// Schedule the price alert checker to run every 6 hours inside the worker service
+cron.schedule("0 */6 * * *", () => {
+  console.log("[worker] Cron triggered: Checking and sending price alerts...");
+  checkAndSendPriceAlerts();
+});
+
 console.log("Scrape worker running...");
+console.log("Price alert background cron worker scheduled in worker.js.");
